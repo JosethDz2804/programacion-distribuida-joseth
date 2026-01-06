4affaaa618291596ba14c3475fcefa1f50d419c3
Joseth Diaz <josethd@tecnologicoismac.edu.ec>
2026-01-05 21:02:50 -0500

    Intento Union con GITHUB

--- src/main/java/com/example/distribuida/Servicelmpl/AutorServiceImpl.java
+++ src/main/java/com/example/distribuida/Servicelmpl/AutorServiceImpl.java
@@ -5,7 +5,6 @@ import com.example.distribuida.model.Autor;
 import com.example.distribuida.Servicelmpl.AutorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
