f7605dad8ad8e54ee1c1448a1ba798a36ae3339f
Joseth Diaz <josethd@tecnologicoismac.edu.ec>
2026-01-05 21:02:50 -0500

    Intento Union con GITHUB

--- src/main/java/com/example/distribuida/Servicelmpl/AutorService.java
+++ src/main/java/com/example/distribuida/Servicelmpl/AutorService.java
@@ -1,12 +1,9 @@
 package com.example.distribuida.Servicelmpl;
-
 import com.example.distribuida.model.Autor;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface AutorService {
-
     List<Autor> findAll();
     Optional<Autor> findById(Integer id);
     Autor save(Autor autor);
