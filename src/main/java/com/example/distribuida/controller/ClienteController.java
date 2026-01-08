a9d3a4760c2f8a74bdf4efde9b898e73770d7e70
Joseth Diaz <josethd@tecnologicoismac.edu.ec>
2026-01-07 21:21:51 -0500

    Creacion de los Controller

---
+++ src/main/java/com/example/distribuida/controller/ClienteController.java
@@ -0,0 +1,56 @@
+package com.example.distribuida.controller;
+
+import com.example.distribuida.Servicelmpl.ClienteService;
+import com.example.distribuida.model.Cliente;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> findAll(){
+        List<Cliente> clientes = clienteService.findAll();
+        return ResponseEntity.ok(clientes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> findOne(@PathVariable int id){
+        Optional<Cliente> cliente = clienteService.findById(id);
+        if(cliente == null ){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente.orElse(null));
+    }
+
+    @PostMapping
+    public  ResponseEntity<Cliente> save(@RequestBody Cliente cliente){
+        Cliente clienteNuevo = clienteService.save(cliente);
+        return ResponseEntity.ok(clienteNuevo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente>update(@PathVariable int id,@RequestBody Cliente cliente){
+        Cliente clienteActualizado = clienteService.update(id, cliente);
+        if(clienteActualizado == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(clienteActualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id){
+        clienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
